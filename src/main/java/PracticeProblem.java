430a39870c4fdf8bc0ce1f846bd0c6ca457d3eb4
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-04-04 23:34:23 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,13 +1,10 @@
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-
-	}
 	public static int sum2D(int[][] array){
 		int sum = 0;
 		for (int row = 0; row < array.length; row++){
@@ -19,10 +16,6 @@ public class PracticeProblem {
 
 	}
 
-
-	public static void q2() {
-		//Write question 2 code here
-	}
 
 	public static int sumRow(int[][] array, int row){
 		int sum = 0;
@@ -38,19 +31,20 @@ public class PracticeProblem {
 	}
 
 
-	public static void q3() {
-		//Write question 3 code here
-	}
 
 	public static int sumColumn(int[][] array, int column){
 		int total = 0;
-		if (array.length > 0 && column >= 0 && column < array[0].length){
-			for (int c = 0; c< array.length; c++){
-				total+= array[c][column];
+		for (int row = 0; row < array.length; row++){
+			for (int col = 0; col< array[row].length; col++){
+				if (col == column){
+					total += array[row][col];
+				}
+			}
 		}
+		
 		return total;
 	}
 
 
 }
-}
+
